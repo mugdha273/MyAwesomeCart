1c79fc17538f3be82f2fd7f592cac8f197088dcf
mugdha273 <mugdhasharma0327@gmail.com>
2021-09-02 13:57:31 +0530

    added_views

--- mac/mac/urls.py
+++ mac/mac/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('shop/', include('shop.urls')),
-    path('blog/', include('blog.urls'))
+    path('blog/', include('blog.urls')),
+
 
 ]
--- mac/shop/urls.py
+++ mac/shop/urls.py
@@ -2,5 +2,12 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path("",views.index, name="ShopHome")
+    path("",views.index, name="ShopHome"),
+    path('about/', views.about,name="AboutUs"),
+    path('tracker', views.tracker,name="TrackingStatus"),
+    path('contact/',views.contact, name="Contactus"),
+    path('search/', views.search,name="Search"),
+    path('productview/', views.productView,name="ProductView"),
+    path('checkout/', views.checkout,name="Checkout"),
+
 ]
