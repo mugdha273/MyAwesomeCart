9c65ba35d6fabd794cc6dc583ab61ef73ef7f4b5
mugdha273 <mugdhasharma0327@gmail.com>
2021-09-03 15:52:30 +0530

    added layout of shop landing page

--- mac/shop/models.py
+++ mac/shop/models.py
@@ -3,6 +3,6 @@ from django.db import models
 # Create your models here.
 class Product(models.Model):
     product_id= models.AutoField
-    product_name= models.CharField(max_length=50)
+    product_name= models.CharField(max_length=1000)
     desc= models.CharField(max_length=300)
     pub_date= models.DateField()
